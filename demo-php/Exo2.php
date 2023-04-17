dd26985fd87b0488caf56ab7f4daf09cbf9b26c8
=kvnlblc <kvnlblc@outlook.fr>
2023-04-17 16:10:32 +0200

    Ajout de fichiers

---
+++ demo-php/Exo2.php
@@ -0,0 +1,9 @@
+<?php
+        $nombre1 = Readline("Entre le premier nombre:");
+        $nombre2 = Readline("Entre le deuxième nombre:");
+        if($nombre1 > 0 and $nombre2 > 0){
+            echo("Positif");
+        }else{
+            echo("Negatif");
+        }
+    ?>
