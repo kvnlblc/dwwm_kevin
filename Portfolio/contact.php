eddf2ff0e9ec67ec1fb451c881f6ddb66e1a6d26
kvnlblc <128465333+kvnlblc@users.noreply.github.com>
2023-08-18 11:15:26 +0200

    Ajout de fichiers

---
+++ Portfolio/contact.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <title>Me contacter</title>
+</head>
+<body>
+    <p class="request_message">
+        Message envoyé !
+    </p>
+
+    <form action="" method="POST">
+        <h2>Me contacter</h2>
+        <input type="text" name="name" placeholder="Votre nom" required>
+        <input type="email" name="email" placeholder="Votre email" required>
+        <textarea name="message" placeholder="Votre message" required></textarea>
+        <input type="submit" value="Envoyer">
+    </form>
+    
+</body>
+</html>
